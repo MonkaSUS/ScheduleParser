d3c621b0195caaa67758961eecd11eadd098fa80
ОБПОУ КГПК <kgpk2@outlook.com>
2023-12-12 12:51:46 +0300

    Добавьте файлы проекта.

---
+++ ScheduleParser/Program.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using ScheduleParser;
+using System.Collections.ObjectModel;
+using System.Data;
+
+const string BaseUrl = "https://schedule.kg-college.ru/";
+ChromeDriver baseDriver = new ChromeDriver();
+
+void GoToScheduleForGroup(string GroupName, ChromeDriver driver)
+{
+    
+    driver.Url = BaseUrl;
+    driver.FindElement(By.XPath("/html/body/div[4]/div/button[1]")).Click();
+    List<IWebElement> Groups = new();
+    for (int i = 1; i <= 104; i++)
+    {
+        Groups.Add(driver.FindElement(By.XPath($"//*[@id=\"selectgroupname\"]/option[{i}]")));
+    }
+    var group = Groups.FirstOrDefault(x => x.Text == GroupName);
+    if (group is not null)
+    {
+        group.Click();
+        driver.FindElement(By.XPath("//*[@id=\"fGroupCase\"]/div[2]/button[2]")).Click();
+    }
+    GetScheduleForThisWeek(driver);
+}
+void GetScheduleForThisWeek(ChromeDriver driver)
+{
+    List<IWebElement> WeekDays = new();
+    IWebElement mainContainer = driver.FindElement(By.XPath("/html/body/div[4]/div"));
+    for (int i = 1; i <= 5; i++)
+    {
+    WeekDays.Add(mainContainer.FindElement(By.XPath($"/html/body/div[4]/div/div[{i}]")));
+    }
+    foreach (var item in WeekDays)
+    {
+        string WeekDayName = item.FindElement(By.XPath("/html/body/div[4]/div/div[1]/div/div[1]/h5/text()")).Text.Split('/')[0];
+        string Date = item.FindElement(By.XPath("/html/body/div[4]/div/div[1]/div/div[1]/h5/text()")).Text.Split('/')[1];
+        List<IWebElement> lessons = new();
+        for (int i = 1; i <= 7; i++)
+        {
+            lessons.Add(item.FindElement(By.XPath($"/html/body/div[4]/div/div[1]/div/div[2]/table/tbody/tr[{i}]")));
+        }
+    }
+}
+GoToScheduleForGroup("ИС-44к", baseDriver);
+
---
+++ ScheduleParser/ScheduleModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleParser
+{
+    public class StudyDay
+    {
+        /// <summary>
+        /// Массив пар дня. Размер фиксирован на 7, чтобы сохранить всем рассужлк. Последние два дня надо инициализировать пустыми и 
+        /// </summary>
+        Lesson?[] Lessons = new Lesson?[7];
+
+        /// <summary>
+        /// Представляет день недели. Индексация с 1 до 7 
+        /// </summary>
+        public DayOfWeek? dayofweek { get; init; }
+        /// <summary>
+        /// Сама дата дня
+        /// </summary>
+        public DateOnly? date { get; init; }
+        
+        /// <summary>
+        /// Если день выходной, то эта перменная должна быть true
+        /// </summary>
+        public bool? IsShabash = false;
+    }
+    /// <summary>
+    /// Класс недели.
+    /// </summary>
+    public class WeekSchedule
+    {
+        public StudyDay[] studyDays;
+        public WeekSchedule(StudyDay[] days)
+        {
+            if (days.Length != 6)
+            {
+                throw new ArgumentException($"Массив дней должен содержать в себе семь элементов, а содержал {days.Length}");
+            }
+            else
+            {
+            studyDays = days;
+
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Класс пары
+    /// </summary>
+    public record Lesson
+    {
+        /// <summary>
+        /// Имя преподавателя
+        /// </summary>
+        public string? TeacherName;
+        /// <summary>
+        /// Номер кабинета
+        /// </summary>
+        public int? ClassroomNumber;
+        /// <summary>
+        /// Название пары
+        /// </summary>
+        public string? LessonName;
+        
+    }
+    /// <summary>
+    /// Дни недели. Индексация с 1 до 7.
+    /// </summary>
+    public enum DayOfWeek
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday= 3,
+        Thursday= 4,
+        Friday= 5,
+        Saturday= 6,
+        Sunday= 7
+    }
+}
